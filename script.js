b5f8b6dd6d5e568291e18c935e682f68b80c477f
pranavbhagawat2763-tech <pranavbhagawat2763@gmail.com>
2026-02-01 21:30:55 +0530

    Industrial Visit Registration Form

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("visitForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let roll = document.getElementById("roll").value;
+    let company = document.getElementById("company").value;
+    let date = document.getElementById("date").value;
+
+    if (name === "" || roll === "" || company === "" || date === "") {
+        alert("All fields are mandatory!");
+    } else {
+        alert("Registration Successful!");
+    }
+});
